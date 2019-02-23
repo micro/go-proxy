961236a8d18832baa2a6ea457da751facf74eb96
Asim Aslam <asim@aslam.me>
2019-02-23 17:17:02 +0000

    fix example

--- examples/proxy/proxy/main.go
+++ examples/proxy/proxy/main.go
@@ -2,12 +2,12 @@ package main
 
 import (
 	"github.com/micro/go-micro"
-	"github.com/micro/go-proxy/router/proxy"
+	"github.com/micro/go-proxy/router/mucp"
 )
 
 func main() {
 	// create a new proxy
-	p := proxy.NewService(
+	p := mucp.NewService(
 		micro.Name("go.micro.proxy"),
 	)
 	// initialise
